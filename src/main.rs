18209f65bc1042799ae77304a8b52427879e30a5
Jonathan Rico <jonathan@rico.live>
2025-12-07 15:20:48 -0700

    serialize some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-// #[derive(Debug)]
-// pub enum KeyEvent {
-//     Unknown,
-//     KeyDown,
-//     KeyUp,
-//     KeyLeft,
-//     KeyRight,
-//     KeySelect,
-//     KeyBack,
-//     KeyPower,
-// }
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub enum KeyEvent {
+    Unknown,
+    KeyDown,
+    KeyUp,
+    KeyLeft,
+    KeyRight,
+    KeySelect,
+    KeyBack,
+    KeyPower,
+}
 
-// #[derive(Debug)]
-// pub struct InputEvent {
-//     pub key: KeyEvent,
-// }
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub struct InputEvent {
+    pub key: KeyEvent,
+}
+
+use serde::{Serialize, Deserialize};
+use postcard::{from_bytes, to_allocvec};
 
 use tokio::net::TcpStream;
 use tokio::io::AsyncWriteExt;
@@ -21,11 +24,17 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let output: Vec<u8> = to_allocvec(&InputEvent {
+        key: KeyEvent::KeyUp,
+    }).unwrap();
+
     // Connect to a peer
     let mut stream = TcpStream::connect("127.0.0.1:9999").await?;
 
-    // Write some data.
-    stream.write_all(b"hello world!\n").await?;
+    stream.write_all(&output).await?;
+
+    let decoded: InputEvent = from_bytes(&output).unwrap();
+    println!("{:?}", decoded);
 
     Ok(())
 }
