870338aed55048923feb5e717450f6bd7fe9d791
Jonathan Rico <jonathan@rico.live>
2025-12-08 23:04:16 -0700

    tx-rx

---
+++ src/rx.rs
@@ -0,0 +1,55 @@
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub enum KeyEvent {
+    Unknown,
+    KeyDown,
+    KeyUp,
+    KeyLeft,
+    KeyRight,
+    KeySelect,
+    KeyBack,
+    KeyPower,
+}
+
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub struct InputEvent {
+    pub key: KeyEvent,
+}
+
+use serde::{Serialize, Deserialize};
+use postcard::{from_bytes_cobs};
+
+use tokio::net::TcpListener;
+use tokio::io::AsyncReadExt;
+use std::io;
+
+async fn process_socket<T: AsyncReadExt + Unpin>(mut socket: T) {
+    println!("Processing..");
+
+    let mut v: Vec<u8> = Vec::new();
+    loop {
+        if let Ok(byte) = socket.read_u8().await {
+            v.push(byte);
+            println!("RX: {:x} VEC {:?}", byte, v);
+            let decoded: Result<InputEvent, _> = from_bytes_cobs(&mut v.clone());
+            if let Ok(data) = decoded {
+                println!("DECODED: {:?}", data);
+                v.clear();
+                return;
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    println!("hello");
+
+    let listener = TcpListener::bind("127.0.0.1:9999").await?;
+
+    println!("Bound. Waiting for conn..");
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        process_socket(socket).await;
+    }
+}
