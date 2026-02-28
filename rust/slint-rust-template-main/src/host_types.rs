328b3287213fc17f6d4a4b79305e31f253b91dc6
Jonathan Rico <jonathan@rico.live>
2026-02-28 12:20:13 -0700

    r: update list from thread

--- rust/slint-rust-template-main/src/host_types.rs
+++ rust/slint-rust-template-main/src/host_types.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Address {
     address_type: u8,
     address: u64,
--- rust/slint-rust-template-main/src/main.rs
+++ rust/slint-rust-template-main/src/main.rs
@@ -1,10 +1,11 @@
 mod host_types;
+use crate::host_types::{Address};
 
 slint::include_modules!();
 use slint::{ModelRc, SharedString, StandardListViewItem, VecModel};
 use std::rc::Rc;
-use crate::host_types::{Address};
 
+#[derive(Clone)]
 struct Device {
     address: Address,
     rssi: i32,
@@ -29,25 +30,38 @@ fn create_row_from_device(device: &Device) -> ModelRc<StandardListViewItem> {
 
 fn main() {
     let ui = AppWindow::new().unwrap();
-    let devices_rows = Rc::new(VecModel::<ModelRc<StandardListViewItem>>::default());
 
-    ui.set_scan_results(ModelRc::from(devices_rows.clone()));
+    let ui_handle = ui.as_weak();
+    let devices_: Box<Vec<Device>> = Box::new(Vec::new());
+    let devices = Box::leak(devices_);
 
-    let devices_rows_copy = devices_rows.clone();
-    ui.on_start_scan(move || {
-        // In a real app, this might come from a Bluetooth crate
-        let new_device = Device {
-            address: Address::new(1, 0x00aA7DDA7113),
-            rssi: -65,
-            name: "Kitchen Sensor".into(),
-            data: "0x010203".into(),
-            _private_field: 42,
-        };
+    std::thread::spawn(move || {
+        loop {
+            std::thread::sleep(std::time::Duration::from_millis(500));
+            println!("Create device");
 
-        // Use our nice refactored function
-        let row = create_row_from_device(&new_device);
-        devices_rows_copy.push(row);
+            let new_device = Device {
+                address: Address::new(1, 0x00aA7DDA7113),
+                rssi: -65,
+                name: "Kitchen Sensor".into(),
+                data: "0x010203".into(),
+                _private_field: 42,
+            };
+            devices.push(new_device);
+
+            let devs = devices.clone();
+            ui_handle.upgrade_in_event_loop(move |ui| {
+                let devices_rows = Rc::new(VecModel::<ModelRc<StandardListViewItem>>::default());
+                ui.set_scan_results(ModelRc::from(devices_rows.clone()));
+
+                let devices_rows_copy = devices_rows.clone();
+                for device in devs {
+                    let row = create_row_from_device(&device);
+                    devices_rows_copy.push(row);
+                }
+            }).unwrap();
+        }
     });
-
+    
     ui.run().unwrap();
 }
