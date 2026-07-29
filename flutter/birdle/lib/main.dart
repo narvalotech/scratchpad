21825fa27bd19d0e01e0de067f7913a23de39fd4
Jonathan Rico <jonathan@rico.live>
2026-07-29 17:19:10 -0600

    fl: textfield

--- flutter/birdle/lib/main.dart
+++ flutter/birdle/lib/main.dart
@@ -24,6 +24,50 @@ class MainApp extends StatelessWidget {
   }
 }
 
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+  final void Function(String) onSubmitGuess;
+  final TextEditingController _textEditingController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit() {
+    onSubmitGuess(_textEditingController.text.trim());
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                ),
+              ),
+              controller: _textEditingController,
+              autofocus: true,
+              onSubmitted: (_) {
+                _onSubmit();
+              },
+            ),
+          ),
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: const Icon(Icons.arrow_circle_up),
+          onPressed: _onSubmit,
+        ),
+      ],
+    );
+  }
+}
+
 class GamePage extends StatelessWidget {
   GamePage({super.key});
   // This object is part of the game.dart file.
@@ -44,6 +88,11 @@ class GamePage extends StatelessWidget {
               for (final letter in guess) Tile(letter.char, letter.type),
             ],
           ),
+          GuessInput(
+            onSubmitGuess: (guess) {
+              print(guess);     // TODO
+            }
+          )
         ],
       ),
     );
