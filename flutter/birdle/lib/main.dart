2a636bb86349823dff19e9041a8e8c157addb977
Jonathan Rico <jonathan@rico.live>
2026-07-29 00:20:58 -0600

    fl: more stuff

--- flutter/birdle/lib/main.dart
+++ flutter/birdle/lib/main.dart
@@ -18,7 +18,33 @@ class MainApp extends StatelessWidget {
             child: Text('BirdleApp'),
           ),
         ),
-        body: Center(child: Tile('A', HitType.hit),),
+        body: Center(child: GamePage()),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This object is part of the game.dart file.
+  // It manages wordle logic, and is outside the scope of this tutorial.
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (final guess in _game.guesses)
+          Row(
+            spacing: 5.0,
+            children: [
+              for (final letter in guess) Tile(letter.char, letter.type),
+            ],
+          ),
+        ],
       ),
     );
   }
