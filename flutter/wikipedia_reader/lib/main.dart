26b42accedd03ad79b4b6f4bef07013c1f56a2f9
Jonathan Rico <jonathan@rico.live>
2026-08-01 00:17:36 -0600

    fl: article fetching

--- flutter/wikipedia_reader/lib/main.dart
+++ flutter/wikipedia_reader/lib/main.dart
@@ -6,6 +6,51 @@ import 'package:http/http.dart';
 
 import 'summary.dart';
 
+class ArticleViewModel extends ChangeNotifier {
+  final ArticleModel model;
+  Summary? summary;
+  Exception? error;
+  bool isLoading = false;
+
+  ArticleViewModel(this.model) {
+    fetchArticle();
+  }
+
+  Future<void> fetchArticle() async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      summary = await model.getRandomArticleSummary();
+      print('Article loaded: ${summary!.titles.normalized}');// TODO remove
+      error = null;
+    } on HttpException catch (e) {
+      print('error: ${e.message}'); // TODO remove
+      error = e;
+      summary = null;
+    }
+
+    isLoading = false;
+    notifyListeners();
+  }
+}
+
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw const HttpException('Failed to update resource');
+    }
+
+    return Summary.fromJson(jsonDecode(response.body) as Map<String, Object?>);
+  }
+}
+
 void main() {
   runApp(const MainApp());
 }
@@ -15,6 +60,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = ArticleViewModel(ArticleModel());
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Wikipedia Flutter')),
