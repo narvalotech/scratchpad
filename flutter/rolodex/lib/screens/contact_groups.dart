a4cce7cb8a811c020a98a09220890b593cb54e50
Jonathan Rico <jonathan@rico.live>
2026-08-01 17:16:04 -0600

    fl: slivers pt1

--- flutter/rolodex/lib/screens/contact_groups.dart
+++ flutter/rolodex/lib/screens/contact_groups.dart
@@ -1,13 +1,87 @@
 import 'package:flutter/cupertino.dart';
+import '../data/contact_group.dart';
+import '../data/contact.dart';
+import '../main.dart';
 
 class ContactGroupsPage extends StatelessWidget {
   const ContactGroupsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
+    return _ContactGroupsView(
+      selectedListId: 0,
+      onListSelected: (list) {
+        debugPrint(list.toString());
+      },
+    );
+  }
+}
+
+class _ContactGroupsView extends StatelessWidget {
+  const _ContactGroupsView({required this.onListSelected, this.selectedListId});
+
+  final int? selectedListId;
+  final void Function(ContactGroup) onListSelected;
+
+  Widget _buildTrailing(List<Contact> contacts, BuildContext context) {
+    final TextStyle style = CupertinoTheme.of(
+      context,
+    ).textTheme.textStyle.copyWith(color: CupertinoColors.systemGrey);
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(contacts.length.toString(), style: style),
+        const Icon(
+          CupertinoIcons.forward,
+          color: CupertinoColors.systemGrey3,
+          size: 18,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
       backgroundColor: CupertinoColors.extraLightBackgroundGray,
-      child: Center(child: Text('Contact Groups will go here')),
+      child: CustomScrollView(
+        slivers: [
+          CupertinoSliverNavigationBar(largeTitle: Text('Lists')),
+          SliverFillRemaining(
+            child: ValueListenableBuilder<List<ContactGroup>>(
+              valueListenable: contactGroupsModel.listsNotifier,
+              builder: (context, contacLists, child) {
+                const groupIcon = Icon(
+                  CupertinoIcons.group,
+
+                  weight: 900,
+                  size: 32,
+                );
+
+                const pairIcon = Icon(
+                  CupertinoIcons.person_2,
+                  weight: 900,
+                  size: 24,
+                );
+
+                return CupertinoListSection.insetGrouped(
+                  header: const Text('iPhone'),
+                  children: [
+                    for (final ContactGroup contactList in contactLists)
+                    CupertinoListTile(
+                      leading: contactList.id == 0 ? groupIcon : pairIcon,
+                      title: Text(contactList.label),
+                      trailing: _buildTrailing(contactList.contacts, context),
+                      onTap: () => onListSelected(contactList),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
