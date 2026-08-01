8e2282020292f30860bd1df752ed1be9432d4cb8
Jonathan Rico <jonathan@rico.live>
2026-08-01 00:54:33 -0600

    fl: rest of the owl
    maybe use expanded for capping vert image size.
    https://api.flutter.dev/flutter/widgets/Expanded-class.html

--- flutter/wikipedia_reader/lib/main.dart
+++ flutter/wikipedia_reader/lib/main.dart
@@ -51,8 +51,104 @@ class ArticleModel {
   }
 }
 
-void main() {
-  runApp(const MainApp());
+class ArticleView extends StatefulWidget {
+  const ArticleView({super.key});
+
+  @override
+  State<ArticleView> createState() => _ArticleViewState();
+}
+
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({super.key, required this.summary});
+
+  final Summary summary;
+
+  @override
+  Widget build(context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        spacing: 10,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          if (summary.description != null)
+          Text(
+            summary.description!,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+          Text(summary.extract),
+        ],
+      ),
+    );
+  }
+}
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+      super.key,
+      required this.summary,
+      required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary), // TODO: cap this
+          ElevatedButton(
+            onPressed: nextArticleCallback,
+            child: const Text('I\'m so random rawr'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  // todo view model here
+  final viewModel = ArticleViewModel(ArticleModel());
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel.fetchArticle();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Wikipedia Flutter')),
+      body: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, _) {
+          return switch ((
+              viewModel.isLoading,
+              viewModel.summary,
+              viewModel.error,
+          )) {
+            (true, _, _) => const CircularProgressIndicator(),
+            (_, _, final Exception e) => Text('Error: $e'),
+            (_, final summary?, _) => ArticlePage(
+              summary: summary,
+              nextArticleCallback: viewModel.fetchArticle,
+            ),
+            _ => const Text('Oh noes'),
+          };
+        },
+      ),
+    );
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -60,12 +156,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = ArticleViewModel(ArticleModel());
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Wikipedia Flutter')),
-        body: const Center(child: Text('Loading...')),
-      ),
-    );
+    return const MaterialApp(home: ArticleView());
   }
 }
+
+void main() {
+  runApp(const MainApp());
+}
+
