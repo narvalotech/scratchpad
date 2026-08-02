84d9d4b7fb7fcf5a50e1584dc10986a2c6e0c48a
Jonathan Rico <jonathan@rico.live>
2026-08-01 18:44:01 -0600

    fl: continue

--- flutter/rolodex/lib/screens/adaptive_layout.dart
+++ flutter/rolodex/lib/screens/adaptive_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 import 'contact_groups.dart';
+import 'contacts.dart';
 
 const largeScreenMinWidth = 600;
 
@@ -28,9 +29,8 @@ class _AdaptiveLayoutState extends State<AdaptiveLayout> {
 
         if (isLargeScreen) {
           return _buildLargeScreenLayout();
-          
         } else {
-          return const ContactGroupsPage();
+          return ContactListsPage(listId: 0); // New
         }
       },
     );
